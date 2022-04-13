1610c7a6cbb1d0f8015a71479fd14f994bb3d1cc
MohanadOO <moohand220@gmail.com>
2022-04-13 16:51:52 +0300

    Show all users & user logs

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ mongoose.connect(process.env.MONGO_URI, {
 
 const userSchema = new Schema({
   username: { type: String, required: true },
+  log: [
+    {
+      description: { type: String, required: true },
+      duration: { type: Number, required: true },
+      date: { type: Date, default: new Date().toDateString() },
+    },
+  ],
 })
 
 let User = mongoose.model('User', userSchema)
@@ -59,25 +66,67 @@ app.post('/api/users', (req, res) => {
 
 app.post('/api/users/:_id/exercises', (req, res) => {
   const _id = req.body.id
-  const description = req.body.description
-  const duration = req.body.duration
-  const date = req.body.date
-
-  User.findOne({ _id: _id }, (error, data) => {
-    if (data === null) {
+  const date =
+    new Date(req.body.date).toDateString() || new Date().toDateString()
+  run()
+  async function run() {
+    try {
+      const user = await User.where('_id').equals(_id)
+      user[0].log = [
+        ...user[0].log,
+        {
+          description: req.body.description,
+          duration: req.body.duration,
+          date: date,
+        },
+      ]
+      await user[0].save()
+      const logLength = user[0].log.length - 1
       res.json({
-        message: 'User dose not exist',
+        _id: user[0]._id,
+        username: user[0].username,
+        date: user[0].log[logLength].date.toDateString(),
+        duration: user[0].log[logLength].duration,
+        description: user[0].log[logLength].description,
       })
-    } else {
-      return res.json({
-        username: data.username,
-        _id: _id,
-        date: new Date(date).toString(),
-        duration: duration,
-        description: description,
-      })
+    } catch (e) {
+      console.log(e.message)
     }
-  })
+  }
+})
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  const id = req.params._id
+  run()
+  async function run() {
+    const user = await User.where('_id').equals(id)
+    const limit = parseInt(req.query.limit) || user[0].log.length
+    const from = new Date(req.query.from).getTime() || 0
+    const to = new Date(req.query.to).getTime() || new Date().getTime()
+    const logs = user[0].log
+    const fix = logs
+      .filter((value) => {
+        return from < value.date.getTime() && to > value.date.getTime()
+      })
+      .map((i) => {
+        return i
+      })
+    const count = [...fix].splice(0, limit).length
+    res.json({
+      _id: user[0]._id,
+      username: user[0].username,
+      count: count,
+      log: fix.splice(0, limit),
+    })
+  }
+})
+
+app.get('/api/users', (req, res) => {
+  async function findLength() {
+    const user = await User.find({}, { log: 0, date: 0 })
+    res.json(user)
+  }
+  findLength()
 })
 
 app.listen(process.env.PORT || 5500)
